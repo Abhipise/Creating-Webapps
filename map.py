a71853e07857f5828196f592ef8b91675580e13e
Abhipise <apise81@gmail.com>
2019-02-13 14:57:43 +0530

    Initial Commit

---
+++ map.py
@@ -0,0 +1,49 @@
+import folium
+import pandas
+
+data = pandas.read_csv("cities.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev= list(data["PLACE"])
+
+map = folium.Map(location=[21.10,79.09], zoom_start=6, tiles="Mapbox Bright")
+
+fgc = folium.FeatureGroup(name=" Major States of India")
+
+for lt, ln, el in zip(lat, lon, elev):
+    fgc.add_child(folium.Marker(location=[lt, ln], popup=el, icon=folium.Icon(color='green')))
+
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elev):
+   if elev<1000:
+    return 'green'
+   elif 1000<= elev <3000:
+       return 'orange'
+   else:
+       return'red'
+map = folium.Map(location=[21.10,79.09], zoom_start=6, tiles="Mapbox Bright")
+
+fgv = folium.FeatureGroup(name="My Map")
+
+for lt, ln, el in zip(lat, lon, elev):
+     fgv.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, popup=str(el)+" m" ,
+     fill_color=color_producer(el),color= 'grey' , fill_opacity=0.7))
+
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x: {'fillColor':'green' if x['properties']['POP2005']< 1000000
+ else 'orange' if 1000000 <= x['properties']['POP2005'] < 2000000 else 'red'}))
+
+map.add_child(fgc)
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+
+
+map.save("Map1.html")
